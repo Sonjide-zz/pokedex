840c7e85bf83552064a5a3e5b67338675580b5ff
Sonjide Hussain <sonjide.hussain@gmail.com>
2020-05-21 20:01:47 +0100

    added hello for testing

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -4,7 +4,10 @@ import React from 'react';
 function CaughtPokemon() {
   const date = new Date().toLocaleDateString();
   return (
+    <div>
     <p>Caught 0 Pokemon on {date}</p>
+    <p>Hello</p>
+    </div>
   );
 }
 
